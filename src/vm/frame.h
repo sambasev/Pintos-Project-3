34e0752ded5d28b00d145f26f82c3a8a1f1bc6ca
Sambasevam Shanmugam <sambasevam@gmail.com>
2012-11-08 22:53:17 -0800

    Added function prototypes

--- src/vm/frame.h
+++ src/vm/frame.h
@@ -5,14 +5,14 @@
 
 struct frame_table {
   //fr is a hashtable containing frame table entries
-  struct hash fr; 
+  struct hash ft; 
   //Used to keep track of free frames
-  struct bitmap *free_frames; 
+  struct bitmap *bm_frames; 
   //Required for synchronizing read writes to the frame table
   struct lock lock;
 };
 
-struct frame_table_entry {
+struct frame_entry {
   //Pointer to the user page
   void * frame;
   //Use for other info on the frame
@@ -22,5 +22,9 @@ struct frame_table_entry {
 };
 
 
-
+void init_frame_table (size_t frame_cnt);
+void * get_frame (enum palloc_flags flags);
+unsigned frame_hash (const struct hash_elem *p_, void *aux UNUSED);
+bool frame_less (const struct hash_elem *a_, const struct hash_elem *b_, void *aux UNUSED);
+void bitmap_init (struct bitmap *bm, size_t frame_cnt);
 
