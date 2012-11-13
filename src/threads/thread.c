81b5cb9072f6657eeb587b9fa3cb9796f498d552
Sambasevam Shanmugam <sambasevam@gmail.com>
2012-11-13 00:51:03 -0800

    housekeeping..

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -103,7 +103,6 @@ thread_init (void)
   init_thread (initial_thread, "main", PRI_DEFAULT);
   initial_thread->status = THREAD_RUNNING;
   initial_thread->tid = allocate_tid ();
-  /* Requires thread to be running since hash_init uses malloc*/
 }
 
 /* Starts preemptive thread scheduling by enabling interrupts.
@@ -493,7 +492,6 @@ init_thread (struct thread *t, const char *name, int priority)
   list_init(&t->child_list);
   t->cp = NULL;
   t->parent = NO_PARENT;
-
 }
 
 /* Allocates a SIZE-byte frame at the top of thread T's stack and
